a5cc34fca6a77a5c67add38e64002aba19c80940
Gertu Piho <gertu.piho@tktk.ee>
2022-10-26 20:51:51 +0300

    EpicGame

---
+++ Week8EpicGame/Program.cs
@@ -0,0 +1,27 @@
+﻿string folderPath = @"C:\Users\Siim\Documents\GertuPiho\Data";
+string heroFile = "heroes.txt";
+string villainFile = "villains.txt";
+
+string[] heroes = File.ReadAllLines(Path.Combine(folderPath, heroFile));
+string[] villains = File.ReadAllLines(Path.Combine(folderPath, villainFile));
+
+//string[] heroes = { "Harry Potter", "Thor", "Spiderman", "Ironman" };
+//string[] villains = { "Voldemort", "Freddy Krueger", "Gollum", "The Joker", "Captain Hook" };
+string[] weapon = { "magic wand", "super suit", "hammer", "cat", "captain america shield" };
+
+string hero = GetRandomValueFromArray(heroes);
+string heroWeapon = GetRandomValueFromArray(weapon);
+Console.WriteLine($"Today {hero} with {heroWeapon} saves the day!");
+
+
+string villain = GetRandomValueFromArray(villains);
+string villainWeapon = GetRandomValueFromArray(weapon);
+Console.WriteLine($"Today {villain} with {villainWeapon} tries to take over the world!");
+
+static string GetRandomValueFromArray(string[] someArray)
+{
+    Random rnd = new Random();
+    int randomIndex = rnd.Next(0, someArray.Length);
+    string randomStringFromArray = someArray[randomIndex];
+    return randomStringFromArray;
+}
